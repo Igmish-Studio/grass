bfdc84c10a903434b9b2749ce61723783f6350ff
Bogdan Peregon <peregonbw@gmail.com>
2017-08-07 17:22:33 +0300

    test bogdan
    test bogdan descr

--- index.php
+++ index.php
@@ -50,7 +50,52 @@ get_header(); ?>
 
 		</main>
 	</div> -->
-
+<div class="container-bogdan-test">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+</div>
     <!-- карусель1 -->
     <div class="container">
         <section class="demos" id="demos">
